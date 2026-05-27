bc2a260b5eeddc6fffa35b23a95cc55d3a847edd
Leo Borda <lsouzaborda@gmail.com>
2026-05-27 15:46:59 -0300

    Email parceirosedu.org.br

--- base/settings.py
+++ base/settings.py
@@ -208,7 +208,7 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "naoresponderparceiros@gmail.com"
+EMAIL_HOST_USER = "reembolsos@parceirosedu.org.br"
 EMAIL_HOST_PASSWORD = ""
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
